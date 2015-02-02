e7e214da5fc86e953bc547e84cf80320094f7c01
Kyle Hogan <kylehogan@Kyles-MacBook-Air.local>
2015-02-02 13:58:04 -0500

    stuff

---
+++ server.rb
@@ -0,0 +1,22 @@
+require 'sinatra'
+require 'sinatra/reloader'
+require_relative './lib/connection'
+require_relative './lib/purchases'
+require_relative './lib/inventory'
+
+
+
+after do
+  ActiveRecord::Base.connection.close
+end
+
+get '/' do
+
+  erb :index, locals: { inventory: Inventory.all() }
+end
+
+get '/shirt/:id' do
+  selectedShirt = params[:id]
+  erb :show, locals: {shirt: selectedShirt}
+
+end
