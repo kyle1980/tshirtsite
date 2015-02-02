92be5b04b23eaf67b9ec1c70b02f74641729f39b
Kyle Hogan <kylehogan@Kyles-MacBook-Air.local>
2015-02-02 13:55:02 -0500

    stuff

--- lib/purchases.rb
+++ lib/purchases.rb
@@ -1,8 +1,8 @@
 require 'active_record'
 
 class Purchases < ActiveRecord::Base
-  def inventory
-    Inventory.where({id: self.shirt_id})
+  def items
+    item.where({id: self.shirt_id})
   end
   def customers
     Customers.where({id: self.customer_id})
